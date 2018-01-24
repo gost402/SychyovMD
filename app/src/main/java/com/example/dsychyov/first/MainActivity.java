67ea26d076cda3a419840b6320fa6eda28b08c1c
Sychyov Denis <gost402@gmail.com>
2018-01-24 21:13:52 +0300

    Add Fabric

--- app/src/main/java/com/example/dsychyov/first/MainActivity.java
+++ app/src/main/java/com/example/dsychyov/first/MainActivity.java
@@ -8,12 +8,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
