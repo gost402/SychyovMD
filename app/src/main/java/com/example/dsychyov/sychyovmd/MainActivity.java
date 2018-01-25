10ebadc6b3eec0afb228cba376174b5bb3297cb4
Sychyov Denis <gost402@gmail.com>
2018-01-26 01:44:26 +0300

    Add author photo, name and link to guthub

--- app/src/main/java/com/example/dsychyov/sychyovmd/MainActivity.java
+++ app/src/main/java/com/example/dsychyov/sychyovmd/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.dsychyov.sychyovmd;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 import net.hockeyapp.android.CrashManager;
@@ -18,21 +26,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        checkForUpdates();
+        setToolbar();
+        activateLink();
+        roundAuthorImage();
+        addFloatingButtonListener();
+        continuousDeploymentIntegrations();
     }
 
     @Override
@@ -89,4 +89,43 @@ public class MainActivity extends AppCompatActivity {
         UpdateManager.unregister();
     }
 
+    private void continuousDeploymentIntegrations() {
+        Fabric.with(this, new Crashlytics());
+        checkForUpdates();
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void roundAuthorImage() {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.author);
+        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+        roundedBitmapDrawable.setCircular(true);
+
+        ImageView authorPhoto = findViewById(R.id.authorPhoto);
+        authorPhoto.setImageDrawable(roundedBitmapDrawable);
+    }
+
+    private void addFloatingButtonListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void activateLink() {
+        // githubLink has links specified by putting <a> tags in the string
+        // resource.  By default these links will appear but not
+        // respond to user input.  To make them active, you need to
+        // call setMovementMethod() on the TextView object.
+
+        TextView githubLink = findViewById(R.id.githubLink);
+        githubLink.setMovementMethod(LinkMovementMethod.getInstance());
+    }
 }
